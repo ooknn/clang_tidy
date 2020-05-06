db20ff29490fbba8b48018b83c91002ee88064da
ggyyll <im.ggyyll@gmail.com>
2020-05-06 17:01:28 +0800

    Add clang_tidy pass_by_value
    Changes to be committed:
    modified:   .clang-tidy
    modified:   main.cc

--- main.cc
+++ main.cc
@@ -1,15 +1,25 @@
-#include <iostream>
-#include <vector>
+#include <memory>
+#include <string>
+#include <utility>
 
-void modernize_loop() {
-  std::vector<int> items;
-  for (int & item : items) {
-    std::cout << item << std::endl;
-  }
+class PipeLine {
+ public:
+  PipeLine(std::string  cmds) : cmd_lines_(std::move(cmds)) {}
+  ~PipeLine() = default;
+
+ private:
+  std::string cmd_lines_;
+};
+
+template <typename... Types>
+void maybe_unused(Types...) {}
+
+void test_make_shared() {
+  auto cmd_lines = std::make_shared<PipeLine>("ls");
+  maybe_unused(cmd_lines);
 }
 
 int main(int argc, char* argv[]) {
-  (void)argc;
-  (void)argv;
+  maybe_unused(argc, argv);
   return 0;
 }
